f51035a0f315f3eff0208e9b63b61f655d6a44df
Zuzanna Wójtowicz <zuzannawojtowicz42@gmail.com>
2023-07-28 17:08:32 +0200

    feat: add button and slider functionality
    - add buttons functionality (play, pause, stop, go back, go forward)
    - synchronise media player value with time slider value
    - add volume slider functionality with mouse wheel support

--- VideoPlayer/MainWindow.xaml.cs
+++ VideoPlayer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Wpf
 {
@@ -23,9 +25,20 @@ namespace Wpf
     public partial class MainWindow : Window
     {
         private bool isPlaying = false;
+        public delegate void timerTick();
+        DispatcherTimer ticks = new DispatcherTimer();
+        timerTick tick;
+        private TimeSpan skipLength = new TimeSpan(0, 0, 5);
         public MainWindow() 
         {
-            InitializeComponent(); 
+            InitializeComponent();
+            SliderVolume.Value = SliderVolume.Maximum;
+            tick = new timerTick(changeStatus);
+        }
+        void changeStatus()
+        {
+            SliderTime.Value = Player.Position.TotalMilliseconds;
+            Duration.Content = Player.Position;
         }
         private void MenuItem_Open(object sender, RoutedEventArgs e)
         {
@@ -35,8 +48,17 @@ namespace Wpf
                 Player.Source = new Uri(openFileDialog.FileName);
             }
         }
+        private void SeekToMediaPosition(object sender, RoutedPropertyChangedEventArgs<double> args)
+        {
+            if (!isPlaying)
+            {
+                int SliderValue = (int)SliderTime.Value;
+                TimeSpan ts = new TimeSpan(0, 0, 0, 0, SliderValue);
+                Player.Position = ts;
+            }
+        }
         private void MenuItem_Exit(object sender, EventArgs e) { Close(); }
-        private void MenuItem_About(object sender, EventArgs e) { MessageBox.Show("", "About", MessageBoxButton.OK); }
+        private void MenuItem_About(object sender, EventArgs e) { MessageBox.Show("A simple video player", "About", MessageBoxButton.OK); }
         void ClickVideo(object sender, RoutedEventArgs args)
         {
             if (isPlaying)
@@ -50,8 +72,70 @@ namespace Wpf
                 isPlaying = true;
             }
         }
-        private void Element_MediaOpened(object sender, EventArgs e) { }
+        private void Element_MediaOpened(object sender, EventArgs e) 
+        {
+            SliderTime.Maximum = Player.NaturalDuration.TimeSpan.TotalMilliseconds;
+            SliderVolume.Minimum = 0;
+            SliderVolume.Maximum = 100;
+            ticks.Interval = TimeSpan.FromMilliseconds(1);
+            ticks.Tick += ticks_Tick;
+            ticks.Start();
+            if (skipLength > Player.NaturalDuration.TimeSpan) skipLength = Player.NaturalDuration.TimeSpan;
+        }
+        void ticks_Tick(object sender, object e) { Dispatcher.Invoke(tick); }
+        void SliderVolume_Click(object sender, MouseButtonEventArgs args)
+        {
+            Point coord = args.GetPosition(SliderVolume);
+            SliderVolume.Value = (coord.X / 70) * 100;
+            Player.Volume = (double)SliderVolume.Value / 100;
+        }
         private void Element_MediaEnded(object sender, EventArgs e) { Player.Stop(); }
-        void MouseWheelFunc(object sender, MouseWheelEventArgs e) { }
+        private void DurationSlider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            Player.Position = new TimeSpan(0, 0, 0, 0, (int)SliderTime.Value);
+        }
+        private void DurationSlider_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            Player.Stop();
+            isPlaying = false;
+        }
+        void MouseWheelFunc(object sender, MouseWheelEventArgs e) 
+        {
+            SliderVolume.Value += 0.1 * e.Delta;
+            Player.Volume = (double)SliderVolume.Value / 100;
+        }
+        void OnMouseDownPlayMedia(object sender, RoutedEventArgs args)
+        {
+            Player.Play();
+            isPlaying = true;
+        }
+        void OnMouseDownPauseMedia(object sender, RoutedEventArgs args)
+        {
+            Player.Pause();
+            isPlaying = false;
+        }
+        void OnMouseDownStopMedia(object sender, RoutedEventArgs args)
+        {
+            Player.Stop();
+            isPlaying = false;
+        }
+        void OnMouseDownPrevMedia(object sender, RoutedEventArgs args)
+        {
+            Player.Pause();
+            Player.Position -= skipLength;
+            SliderTime.Value -= skipLength.TotalMilliseconds;
+            Player.Play();
+        }
+        void OnMouseDownNextMedia(object sender, RoutedEventArgs args)
+        {
+            Player.Pause();
+            Player.Position += skipLength;
+            SliderTime.Value += skipLength.TotalMilliseconds;
+            Player.Play();
+        }
+        void OnMouseDownMuteMedia(object sender, RoutedEventArgs args)
+        {
+
+        }
     }
 }
